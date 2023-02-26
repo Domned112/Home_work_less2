3d84d44dc1122f5ec5a66be9ece3818cc6268e79
Domned112 <124935303+Domned112@users.noreply.github.com>
2023-02-27 00:37:18 +0300

    Update Main.java

--- src/main/java/ru/psv112/Main.java
+++ src/main/java/ru/psv112/Main.java
@@ -2,6 +2,6 @@ package ru.psv112;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Alex and have a good day!");
+        System.out.println("Hello Alex and have a good day!!");
     }
 }
